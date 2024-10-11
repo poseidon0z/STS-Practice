2751940792c1de661500f888752fee937a60f377
poseidon0z <adityaprabhu1290@gmail.com>
2024-10-11 21:13:40 +0530

    Added max consecutive ones

---
+++ maxConsecutiveOnes.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class maxConsecutiveOnes {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the number");
+        char number[] = in.next().toCharArray();
+
+        System.out.println("Enter the number of flips: ");
+        int flips = in.nextInt();
+
+        int maxOnes = 0;
+        int replacements = 0;
+        int start = 0;
+
+        for (int end = 0; end < number.length; end++) {
+            if (number[end] == '0') {
+                replacements++;
+            }
+            while (replacements > flips) {
+                if (number[start] == '0') {
+                    replacements--;
+                }
+                start++;
+            }
+            maxOnes = Math.max(maxOnes, end - start + 1);
+        }
+
+        System.out.println(maxOnes);
+    }
+}
